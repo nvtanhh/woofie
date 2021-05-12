9e8d16f9395a7e3ccc0e35f98ec74236537aa519
BaoDev <nguyennhubao999@gmail.com>
2021-05-12 17:29:33 +0700

    add ui, add store user

---
+++ lib/modules/auth/data/storages/user_storage.dart
@@ -0,0 +1,18 @@
+import 'package:gii_pos/modules/user/domain/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:suga_core/suga_core.dart';
+
+class UserStorage extends Storage<User> {
+  UserStorage(SharedPreferences prefs, String key) : super(prefs: prefs, key: key);
+
+  @override
+  User? get({User? defaultValue}) {
+    final String? jsonString = prefs.getString(key);
+    return jsonString != null ? User.fromJsonString(jsonString) : defaultValue;
+  }
+
+  @override
+  void set(User value) {
+    prefs.setString(key, value.toJsonString());
+  }
+}
