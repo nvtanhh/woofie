8cb230cbad05e3c7489c218a0328b42d78a49628
BaoDev <nguyennhubao999@gmail.com>
2021-06-08 11:36:30 +0700

    [profile] add events

---
+++ lib/modules/social_network/domain/events/pet/pet_vaccinate_created_event.dart
@@ -0,0 +1,7 @@
+import 'package:meowoof/modules/social_network/domain/models/pet/pet_weight.dart';
+
+class PetWormFlushCreatedEvent{
+  final PetWeight _petWeight;
+
+  PetWormFlushCreatedEvent(this._petWeight);
+}
