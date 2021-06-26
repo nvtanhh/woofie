84b9cf6eca191375e1cd5bdc71d71bb881370b88
Spider Man <66580316+nvtanhh@users.noreply.github.com>
2021-06-26 22:05:06 +0700

    Update register_widget.dart

--- lib/modules/auth/app/ui/register/register_widget.dart
+++ lib/modules/auth/app/ui/register/register_widget.dart
@@ -16,7 +16,8 @@ class RegisterWidget extends StatefulWidget {
   _RegisterWidgetState createState() => _RegisterWidgetState();
 }
 
-class _RegisterWidgetState extends BaseViewState<RegisterWidget, RegisterWidgetModel> {
+class _RegisterWidgetState
+    extends BaseViewState<RegisterWidget, RegisterWidgetModel> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -73,7 +74,8 @@ class _RegisterWidgetState extends BaseViewState<RegisterWidget, RegisterWidgetM
                             controller: viewModel.nameEditingController,
                             validator: (name) => viewModel.nameValidate(name),
                             decoration: InputDecoration(
-                                hintText: LocaleKeys.register_name_account.trans(),
+                                hintText:
+                                    LocaleKeys.register_name_account.trans(),
                                 border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(10.r),
                                   borderSide: const BorderSide(
@@ -115,7 +117,8 @@ class _RegisterWidgetState extends BaseViewState<RegisterWidget, RegisterWidgetM
                           ),
                           TextFormField(
                             controller: viewModel.emailEditingController,
-                            validator: (email) => viewModel.emailValidate(email),
+                            validator: (email) =>
+                                viewModel.emailValidate(email),
                             decoration: InputDecoration(
                                 hintText: LocaleKeys.login_email.trans(),
                                 border: OutlineInputBorder(
@@ -160,7 +163,8 @@ class _RegisterWidgetState extends BaseViewState<RegisterWidget, RegisterWidgetM
                           Obx(
                             () => TextFormField(
                               controller: viewModel.passwordEditingController,
-                              validator: (password) => viewModel.passwordValidate(password),
+                              validator: (password) =>
+                                  viewModel.passwordValidate(password),
                               decoration: InputDecoration(
                                   hintText: LocaleKeys.login_password.trans(),
                                   border: OutlineInputBorder(
@@ -202,11 +206,13 @@ class _RegisterWidgetState extends BaseViewState<RegisterWidget, RegisterWidgetM
                                     onTap: viewModel.onEyeClick,
                                     child: Obx(
                                       () => Icon(
-                                        viewModel.showPassword ? Icons.remove_red_eye_outlined : Icons.remove_red_eye,
+                                        !viewModel.showPassword
+                                            ? Icons.visibility_off_rounded
+                                            : Icons.visibility_rounded,
                                       ),
                                     ),
                                   )),
-                              obscureText: viewModel.showPassword,
+                              obscureText: !viewModel.showPassword,
                             ),
                           ),
                         ],
